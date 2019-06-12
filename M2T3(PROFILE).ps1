81d3eeb9ce5c0feb8251e6cf58b5ac45e133d6d9
PavelPinchuk <51189684+PavelPinchuk@users.noreply.github.com>
2019-06-12 12:04:30 +0300

    3

--- M2T3(PROFILE).ps1
+++ M2T3(PROFILE).ps1
@@ -3,11 +3,10 @@ function profile
 'MyProfile'
 }
 Set-location c:\
-Get-Alias c clear
-Get-Alias gm get-member
-New-Variable a -value 2019
+new-Variable a -value 2019
+set-Alias c clear
+set-Alias gm get-member
  (Get-Host).UI.RawUI.ForegroundColor = 'red'
  (Get-Host).UI.RawUI.BackgroundColor = 'black'
- (Get-Host).UI.RawUI.CursorSize = 17
 Clear-Host
 Get-Module -ListAvailable
